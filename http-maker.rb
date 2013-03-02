10ab697cbb818916d03b46a9dd7788c3ba22a603
Shelvacu Vevevende <shelvacu@gmail.com>
2013-03-02 12:19:20 -0800

    added html-maker

---
+++ http-maker.rb
@@ -0,0 +1,39 @@
+require 'cgi'
+class HTMLMaker
+	
+	def initialize()
+		@currentHTML = ""
+	end
+	
+	def method_missing(method_name,raw_attr = {})
+		raise 'must pass object responding to #each' unless raw_attr.respond_to?(:each)
+		
+		attributes = []
+		raw_attr.each do |key,val|
+			attr = String.try_convert(val)
+			raise if attr.nil?
+			attributes << "#{key}=\"#{CGI::escapeHTML(val)}\""
+		end
+		
+		if block_given?
+			@currentHTML += "<#{method_name.to_s}#{attributes.empty? ? "" : " "+attributes.join(" ")}>"
+			add = String.try_convert(yield)
+			@currentHTML += add unless add.nil?
+			@currentHTML += "</#{method_name.to_s}>"
+		else
+			@currentHTML += "<#{method_name.to_s}#{attributes.empty? ? "" : " "+attributes.join(" ")} />"
+		end
+		return nil
+	end
+	
+	def add_element(name)#,attr = {})
+		name = name.to_sym
+		method_missing(name)#,attr)
+	end
+	
+	def <<(stuff)
+		@currentHTML += String.try_convert(stuff).to_s
+	end
+	
+	def to_s;@currentHTML;end
+end
--- web.rb
+++ web.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require './http-maker'
 
 get '/' do
-	"OMGOMGOMGOMGOMGOMGOMGOMGOMGOMGOMGOMGOMGOMGOMGOMGOMGOMGOMGOMGOMG\n\nHI!"
+	$h = HTTPMaker.new
+	$h.html do
+		$h.body do
+			$h.h1(:id => 'awesome'){"Hello, world"}
+			$h.img(:src => "http://thelazy.info/wp-content/uploads/2010/12/hello-world-2-600x4011.jpg")
+			if rand(10) == 0
+				$h.h1(:style => "font-size:big;"){"It's your lucky day!"}
+			end
+		end
+	end
 end
