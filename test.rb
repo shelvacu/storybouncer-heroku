cd48a2a2e20a6f47c98374e55d54ca46724cf31f
Shelvacu Vevevende <shelvacu@gmail.com>
2013-03-05 23:41:12 -0800

    first version of game; login-out only

--- test.rb
+++ test.rb
@@ -1,9 +1,8 @@
-require 'sequel'
+require 'sinatra'
+require 'pp'
+enable :session
 
-DB =  Sequel.connect(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-#DB.create_table :test do
-#	primary_key :id
-#	String :text
-#end
-tg = 'a'*3_000_000_000
-DB[:test].insert(:text => tg)
+get '/' do
+	pp session
+	"Hi"
+end
