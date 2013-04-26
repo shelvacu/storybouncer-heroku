74800918670568c39379ec528a3a5ad4aaacbcfb
Shelvacu Vevevende <shelvacu@gmail.com>
2013-04-25 20:54:38 -0700

    extra debug for stuff module

--- local_sequel.rb
+++ local_sequel.rb
@@ -1,10 +1,10 @@
 require 'sequel'
 def getarray(id)
-  return DB["array#{id}"]
+  return DB[:"array#{id}"]
 end
 def makearray(type = Integer,name = :val)
-  id = DB['array'].insert
-  DB.create_table("array#{id}") do
+  id = DB[:array].insert
+  DB.create_table(:"array#{id}") do
     primary_key :id
     column name, type
   end
