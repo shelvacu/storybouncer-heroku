2c25557063c87dd95efd35578da8ed1e4afce832
Shelvacu Vevevende <shelvacu@gmail.com>
2013-05-19 03:31:48 -0700

    revert to MRI

--- local_sequel.rb
+++ local_sequel.rb
@@ -12,19 +12,5 @@ def makearray(type = Integer,name = :val)
   end
   return id
 end
-puts "RUBY_ENGINE = #{RUBY_ENGINE}"
-DB = Sequel.connect((RUBY_ENGINE == 'jruby' ? 'jdbc:' : "")+(ENV['JUSTONEDB_DBI_URL'] || 'sqlite:local.db'))
-=begin
-if (db_url = ENV['JUSTONEDB_DBI_URL'])
-  parts = db_url.split("//")
-  protocol = parts[0]
-  user_pass_url = parts[1].split("@")
-  user_pass = user_pass_url[0].split(":")
-  user = user_pass[0]
-  pass = user_pass[1]
-  domain_path = user_pass_url[1]
-  DB = Sequel.connect("jdbc:#{protocol}//#{domain_path}?user=#{user}&password=#{pass}")
-else
-  DB = Sequel.connect('jdbc:sqlite:local.db')
-end
-=end
+DB = Sequel.connect(ENV['JUSTONEDB_DBI_URL'] || 'sqlite://local.db')
+
