c0ceb19d3a491e0ddd3e7b7f37eaffcedb466197
Shelvacu Vevevende <shelvacu@gmail.com>
2013-05-19 18:05:08 -0700

    aweagfewa

--- local_sequel.rb
+++ local_sequel.rb
@@ -12,4 +12,10 @@ def makearray(type = Integer,name = :val)
   end
   return id
 end
-DB = Sequel.connect(ENV['JUSTONEDB_DBI_URL'].gsub("postgres:","jdbc:postgresql") || 'jdbc:sqlite:local.db')
+#DB = Sequel.connect(ENV['JUSTONEDB_DBI_URL'].gsub("postgres:","jdbc:postgresql") || 'jdbc:sqlite:local.db')
+if (url = ENV['JUSTONEDB_DBI_URL'])
+  m = url.match(/:\/\/(?<user>\w+):(?<pass>\w+)@(?<else>.*)/)
+  DB = Sequel.connect("jdbc:posgresql://#{m[:else}?user=#{m[:user]}&pass=#{m[:pass]}")
+else
+  DB = Sequel.connect("jdbc:sqlite:local.db")
+end
